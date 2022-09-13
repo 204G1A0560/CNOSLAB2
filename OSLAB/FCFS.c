16f1a2fca4fbb901a6e2145d0505864e23f96c21
Mounesh <94514583+204G1A0560@users.noreply.github.com>
2022-09-13 12:44:30 +0530

    FCFS.c
    I completed FCFS program

--- OSLAB/FCFS.c
+++ OSLAB/FCFS.c
@@ -5,30 +5,31 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-int main(){
-<<<<<<< HEAD
-int bt[20],wt[20],tat[20],i,n;
-=======
-int bt[20],wt[20],tat[20],1,n;
->>>>>>> refs/remotes/origin/master
-  float wtavg,tatavg;
-  printf("\nEnter the number of processes--");
-  scanf("%d",&n);
-  for(i=0;i<n;i++){
-  printf("\nEnter Bursting Time for process");
-  scanf("%d",&bt[i]);}
-  wt[0]=wtavg=0;
-  tat[0]=tatavg=bt[0];
-  for(i=0;i<n;i++){
-    wt[i]=wt[i-1]+bt[i-1];
-    tat[i]=tat[i-1]+bt[i];
-    wtavg=wtavg+wt[i];
-    tatavg=tatavg+tat[i];
-                  }
-  printf("\tPROCESS\tBURST TIME\tWAITING TIME\tTURN AROUND TIME\n");
-  for(i=0;i<n;i++)
-    printf("\n\tP%d\t\t%d\t\t%d\t\t%d",i,bt[i],wt[i],tat[i]);
-  printf("\n Average waiting Time--%f ",wtavg/n);
-  printf("\n Average Turn Around Time--%f",tatavg/n);
-  return 0;
+int main()
+{
+int bt[20], wt[20], tat[20], i, n;
+float wtavg, tatavg;
+
+printf("\nEnter the number of processes -- ");
+scanf("%d", &n);
+for(i=0;i<n;i++)
+{
+printf("\nEnter Burst Time for Process %d -- ", i);
+scanf("%d", &bt[i]);
+}
+wt[0] = wtavg = 0;
+tat[0] = tatavg = bt[0];
+for(i=1;i<n;i++)
+{
+   wt[i] = wt[i-1] +bt[i-1];
+tat[i] = tat[i-1] +bt[i];
+wtavg = wtavg + wt[i];
+tatavg = tatavg + tat[i];
+}
+printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
+for(i=0;i<n;i++)
+printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
+printf("\nAverage Waiting Time -- %f", wtavg/n);
+printf("\nAverage Turnaround Time -- %f", tatavg/n);
+return 0;
 }
