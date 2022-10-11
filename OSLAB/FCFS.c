24b8eac567f547b76b29a976746c2b5b9019ded3
Mounesh <94514583+204G1A0560@users.noreply.github.com>
2022-10-11 11:17:53 +0530

    FCFS.c
    I completed FCFS.c

--- OSLAB/FCFS.c
+++ OSLAB/FCFS.c
@@ -5,11 +5,11 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-int main()
+main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-
+clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -21,7 +21,8 @@ wt[0] = wtavg = 0;
 tat[0] = tatavg = bt[0];
 for(i=1;i<n;i++)
 {
-   wt[i] = wt[i-1] +bt[i-1];
+
+wt[i] = wt[i-1] +bt[i-1];
 tat[i] = tat[i-1] +bt[i];
 wtavg = wtavg + wt[i];
 tatavg = tatavg + tat[i];
@@ -31,5 +32,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-return 0;
 }
